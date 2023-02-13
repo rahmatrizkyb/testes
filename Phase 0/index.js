1333768319d117c4ad97edbb1188e3028157abe6
Rahmat Rizki <rahmatrizkyb@Rahmats-Air.localdomain>
2023-02-13 15:49:09 +0700

    testesaja

---
+++ Phase 0/index.js
@@ -0,0 +1,77 @@
+// console.log ("Hello World!");
+
+// variable 
+// let gelas = 'kopi' // deklarasi dan assigment 
+// console.log (gelas) 
+
+// gelas = "susu"  // re - assigment
+// console.log (gelas)
+
+// let perhitungan = 70 * 10 + 7 * 0 + 1
+// //                  700   +   0    + 1
+// // console.log (perhitungan)
+
+// // tipe data primitives => tipe data yang sederhana
+// // number => tipe data yang menampung angka
+// let angka = 5
+// // string => tipe data yang menampung kata / kalimat dengan / "" /''/``/
+// let nama = "Michael"
+// //          0123456
+// console.log (nama [4])
+
+// // boolean => tipe data yang berisi true/ false
+// let isMarried = false
+// let data = "9"
+
+
+// // typeof=> digunakan untuk cek tipe data dari sebuah variabel
+// console.log (typeof angka)
+// console.log (typeof nama)
+// console.log (typeof isMarried, isMarried)
+// console.log (typeof data)
+
+// let number1 = 21
+// let number2 = 4
+// console.log (number1+number2)
+// console.log (number1-number2)
+// console.log (number1*number2)
+// console.log (number1/number2)
+// console.log (number1%number2)
+
+
+// menghitung luas segitiga ( rumus alas + tinggi / 2)
+// input 
+let alas = 10
+let tinggi = 5
+
+// proses
+let luas = alas + tinggi / 2
+
+// output
+console.log (luas)
+
+// ===
+//==
+//!==
+//!=
+// >
+//
+
+
+let nilai1 = 10
+let nilai2 = 5
+
+console.log (nilai1 > nilai2)
+console.log (nilai < nilai2)
+
+
+// jika saya lapar maka saya akan makan, jika saya tidak lapar makan saya akan minum
+// input
+let lapar = true
+
+// proses
+if (lapar === true) {
+    console.log ("saya akan makan")
+} else {
+    console.log ("saya akan minum")
+}
